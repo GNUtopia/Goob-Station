9655b2c4f049812437709b68af7dad600b3d1be3
GNUtopia <erainach14@gmail.com>
2025-09-21 14:55:15 -0500

    guehhh

---
+++ Content.Goobstation.Server/Surgery/RobotSurgeryComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Goobstation.Server.Surgery;
+
+/// <summary>
+///     Marks limbs as using robot surgeries instead of standard surgeries.
+/// </summary>
+///
+[RegisterComponent]
+public sealed partial class RobotSurgeryComponent : Component
+{
+}
